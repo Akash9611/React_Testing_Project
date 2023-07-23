403d745c7f0f81c78e8feb23cf18b5eb7051b6a1
unknown <gpkp176172@gmail.com>
2023-07-23 23:06:15 +0530

    2]Nested Describe Blocks

--- src/components/greet/Greet.test.js
+++ src/components/greet/Greet.test.js
@@ -1,7 +1,7 @@
-import { render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-//! Simple Testing Method
+////! Simple Testing Method
 // test('greeting component test', () =>{
 //     render(<Greet/>);
 //     const testGreet = screen.getByText(/hello/i); // i - is for testing with string Case-insensitive 
@@ -16,26 +16,43 @@ import Greet from './Greet';
 // })
 
 
-// ---------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
 
 
-//? Topics
-//! 1] To run 'only' particular Test Case or To 'skip' particular Test Case 
-//* i) only ii) skip
+////? Topics
+////! 1] To run 'only' particular Test Case or To 'skip' particular Test Case 
+////* i) only ii) skip
 // test.only('greeting component test', () =>{ //todo: it will only run this Test Case and skip all other Tests Cases
 // test.skip('greeting component test', () =>{ //todo: it will only skip this Test Case and run all other Test Cases
 
-//! 2] Grouping multiple Test Cases into one Test Case Using 'describe()'
-//* i) describe() 
-describe('Greet', ()=>{
-    test('component test', ()=>{
-        render(<Greet/>);
+////! 2] Grouping multiple Test Cases into one Test Case Using 'describe()'
+////* i) describe() 
+// describe('Greet', ()=>{
+//     test('component test', ()=>{
+//         render(<Greet/>);
+//         const testGreet = screen.getByText('Hello');
+//         expect(testGreet).toBeInTheDocument();
+//     })
+//     test('name props', ()=>{
+//             render(<Greet name='Akash'/>);
+//     const testGreetProps = screen.getByText('Hello Akash');  
+//     expect(testGreetProps).toBeInTheDocument(); 
+//     })
+// });
+
+////* ii)we can also create Nested describe blocks 
+describe('Greet', () => {
+    test('component test', () => {
+        render(<Greet />);
         const testGreet = screen.getByText('Hello');
         expect(testGreet).toBeInTheDocument();
     })
-    test('name props', ()=>{
-            render(<Greet name='Akash'/>);
-    const testGreetProps = screen.getByText('Hello Akash');  
-    expect(testGreetProps).toBeInTheDocument(); 
+
+    describe('Nested Greet', () => {
+        test('name props', () => {
+            render(<Greet name='Akash' />);
+            const testGreetProps = screen.getByText('Hello Akash');
+            expect(testGreetProps).toBeInTheDocument();
+        })
     })
-});
+})
