e1c6c9fee7cb7ed29a863c966fda6a8f059a05ec
unknown <gpkp176172@gmail.com>
2023-07-27 23:57:48 +0530

    Applications Component and Aplications component Test updated- Learning more about getByRole options

--- src/applications/Applications.js
+++ src/applications/Applications.js
@@ -1,12 +1,51 @@
-import React from "react";
+//!Applications Code 1
+// import React from "react";
+// const Applications = () => {
+//     return (
+//         <div>
+//             <form>
+//                 <div>
+//                     <label htmlFor="name">Name</label>
+//                     <input type="text" id="name" />
+//                 </div>
+//                 <div>
+//                     <label htmlFor="job-location">Job-Location</label>
+//                     <select id="job-Location">
+//                         <option value="">select job-location</option>
+//                         <option value="IND">India</option>
+//                         <option value="US">United State</option>
+//                         <option value="CND">Canada</option>
+//                         <option value="AUS">Australia</option>
+//                     </select>
+//                 </div>
+//                 <div>
+//                     <label>
+//                         <input type='checkbox' id='terms'/>I agree to the terms and conditions
+//                     </label>
+//                 </div>
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </div>
+//     );
+// }
 
+// export default Applications;
+
+//!Applications Code 2
+import React from "react";
 const Applications = () => {
     return (
-        <div>
+        <>
+            <h1>Applications Submit Form </h1> {/*heading 1 */}
+            <h2>Sections</h2> {/*heading 2 */}
             <form>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" />
+                    <input type="text" id="name" /> {/*input text 1 */}
+                </div>
+                <div>
+                    <label htmlFor="bio">Bio</label>
+                    <textarea id='bio' name='bio' /> {/*input text2 It is same as input text in react */}
                 </div>
                 <div>
                     <label htmlFor="job-location">Job-Location</label>
@@ -20,12 +59,12 @@ const Applications = () => {
                 </div>
                 <div>
                     <label>
-                        <input type='checkbox' id='terms'/>I agree to the terms and conditions
+                        <input type='checkbox' id='terms' />I agree to the terms and conditions
                     </label>
                 </div>
                 <button type="submit">Submit</button>
             </form>
-        </div>
+        </>
     );
 }
 
--- src/applications/Applications.test.js
+++ src/applications/Applications.test.js
@@ -2,10 +2,53 @@ import { render, screen } from '@testing-library/react';
 import Applications from './Applications';
 
 describe('Application', () => {
+    //!Applications Code 1
+    // test('All fileds', () => {
+    //     render(<Applications />);
+    //     const nameElement = screen.getByRole('textbox'); //checking name text input is present
+    //     expect(nameElement).toBeInTheDocument();
+
+    //     const dropdownElement = screen.getByRole('combobox'); // checking job-locations dropdown list is present
+    //     expect(dropdownElement).toBeInTheDocument();
+
+    //     const checkboxElement = screen.getByRole('checkbox'); // checking terms checkbox input is present
+    //     expect(checkboxElement).toBeInTheDocument();
+
+    //     const buttonElement = screen.getByRole('button'); // checking button is present
+    //     expect(buttonElement).toBeInTheDocument();
+    // })
+
+    //!Applications Code 2
     test('All fileds', () => {
         render(<Applications />);
-        const nameElement = screen.getByRole('textbox'); //checking name text input is present
+        
+        //?getByRole Options
+        //todo: 1] 'name' option
+        // //!h1
+        // const headingElement1 = screen.getByRole('heading', { name: 'Applications Submit Form' });//if we have a multiple headings tag then we should have to define the 'name' option(else test will fail) in the getByRole() and pass the text inside of the heading to the option;
+        // expect(headingElement1).toBeInTheDocument();
+
+        // //!h2
+        // const headingElement2 = screen.getByRole('heading', {name:'Sections'});
+        // expect(headingElement2).toBeInTheDocument();
+
+        //todo: 2]'level' option
+        //h1
+        const headingElement1 = screen.getByRole('heading', { level:1 });//if we have a multiple headings tag then we should have to define the 'level' option(else test will fail) in the getByRole() and level as number(level option accept only 6 level);
+        expect(headingElement1).toBeInTheDocument();
+
+        //h2
+        const headingElement2 = screen.getByRole('heading', {level:2});
+        expect(headingElement2).toBeInTheDocument();
+
+        //Name input
+        const nameElement = screen.getByRole('textbox', { name: 'Name' }); //if we have a multiple same text input filed then we should have to define the 'name' option(else test will fail) in the getByRole() and pass the carousing/binding label text as name 
         expect(nameElement).toBeInTheDocument();
+
+        //Bio input
+        const bioElement = screen.getByRole('textbox', { name: 'Bio' });
+        expect(bioElement).toBeInTheDocument();
+
 
         const dropdownElement = screen.getByRole('combobox'); // checking job-locations dropdown list is present
         expect(dropdownElement).toBeInTheDocument();
