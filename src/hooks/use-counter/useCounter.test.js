9b14c2405ea8b8ad4405307a72725dc8269e1dac
Akash9611 <gkpkp176172@gmail.com>
2023-08-02 15:53:12 +0530

    act()=>Learned about act() to perform asserting of function

--- src/hooks/use-counter/useCounter.test.js
+++ src/hooks/use-counter/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useCounter } from './UseCounter';
 
 describe('Custom Hook Testing', () => {
@@ -9,8 +9,28 @@ describe('Custom Hook Testing', () => {
 
     test('should accept and render the same initial count', () => {
         const { result } = renderHook(useCounter, {
-            initialProps:  10
+            initialProps: 10
         })
         expect(result.current.count).toBe(10)
+    })
+    test('should increment the count', () => {
+        const { result } = renderHook(useCounter);
+        // const increment = () => setCount(count+1); code
+
+        // result.current.increment(); // if call method like this it was not going the perform or update the count ; to perform the assert on the output we should have to use act()
+        //todo: act()
+        // act(() => {
+        //     /* fire events that update state */
+        //   });
+        //   /* assert on the output */
+        act(() => result.current.increment());
+        expect(result.current.count).toBe(1)
+    })
+
+    test('should decrement the count', () => {
+        const { result } = renderHook(useCounter);
+        // const decrement = () => setCount(count-1); code
+        act(() => result.current.decrement());
+        expect(result.current.count).toBe(-1)
     })
 })
