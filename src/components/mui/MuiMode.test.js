632b2bf5f8fb863669fa61f0b6064d08448e8639
Akash9611 <gkpkp176172@gmail.com>
2023-08-02 14:42:09 +0530

    AppProvider in testing method to with using test-utils.js Custom Render

--- src/components/mui/MuiMode.test.js
+++ src/components/mui/MuiMode.test.js
@@ -1,11 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import AppProvider from '../../provider/app-provider';
+//!Method 1 
+// import { render, screen } from '@testing-library/react';
+// import AppProvider from '../../provider/app-provider';
+// import MuiMode from './MuiMode';
+
+// describe('MuiMode Component', () => {
+//     test('render test correctly', () => {
+//         render(<MuiMode />, { wrapper: AppProvider }); //todo: method-1] if component is wrapped into the AppProvider we should have use this type of wrapper option to run the tests, if not used then test will get failed
+//         const headingElement = screen.getByRole('heading');
+//         expect(headingElement).toHaveTextContent("dark mode");
+//     })
+
+// })
+
+//! Method 2- Test getting AppProvider predefined to wrapper with test and options from ['test-utils.js'] file 
+import { render, screen } from '../../test-utils' //make sure to import methods from test-utils.js file
 import MuiMode from './MuiMode';
 
 describe('MuiMode Component', () => {
     test('render test correctly', () => {
-        render(<MuiMode />, { wrapper: AppProvider }); // method-1] if component is wrapped into the AppProvider we should have use this type of wrapper option to run the tests, if not used then test will get failed
+        render(<MuiMode />); // method-2]getting/importing render and wrapper with AppProvider from the 'test-utils.js' file
         const headingElement = screen.getByRole('heading');
         expect(headingElement).toHaveTextContent("dark mode");
     })
+
+
 })
