3a7eab3326b83d94e5296975486f3b5c6c11cff4
Akash9611 <gkpkp176172@gmail.com>
2023-08-02 16:33:54 +0530

    setuping Mock Service Worker(MSW) for Mocking data

--- src/mocks/sever.js
+++ src/mocks/sever.js
@@ -1,3 +1,6 @@
+//use the following website to install and setup the Mock Service Worker(MSW)
+// https://mswjs.io/docs/getting-started/install 
+
 // src/mocks/server.js
 import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
