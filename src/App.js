9b780879816abaa01ecde3bc56a39b13a52faea3
Akash9611 <gkpkp176172@gmail.com>
2023-08-02 16:33:48 +0530

    setuping Mock Service Worker(MSW) for Mocking data

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import Applications from './components/applications/Applications';
 import Count from './components/counter/Count';
 import MuiMode from './components/mui/MuiMode';
 import Skill from './components/skills/Skill';
+import Users from './components/users/Users';
 import AppProvider from './provider/app-provider';
 
 function App() {
   return (
        
-    //   <div className="App">
-    //     <Applications/>
-    //     <Skill/>
-    //     <Count/> 
-    //  </div>
-      //! ### Testing with App providers
-    <AppProvider>
       <div className="App">
-        <MuiMode />
-      </div>
-    </AppProvider>
+        {/* <Applications/>
+        <Skill/>
+        <Count/>  */}
+        <Users/>
+     </div>
+      //! ### Testing with App providers
+    // <AppProvider>
+    //   <div className="App">
+    //     <MuiMode />
+    //   </div>
+    // </AppProvider>
   );
 }
 
